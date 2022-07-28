8d5762ee1baba6cf99224ecc2ab81741b7a023cc
Raphael Zimeray <raphaelzimeray@me.com>
2022-07-28 20:47:18 +0200

    v1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Bookmark.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+puts "Creating movies..."
+
+list = List.new(name: "Liste de films pour test")
+
+
+wonder = {title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9}
+redemption = {title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7}
+titanic = {title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9}
+eight = {title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0}
+
+[wonder, redemption, titanic, eight].each do |movie|
+  movie = Movie.create!(movie)
+  puts "Created #{movie.title}"
+end
+list.save!
+
+
+Bookmark.create!(comment: "top genial!", movie: Movie.find_by(title: "Wonder Woman 1984"), list: list)
+Bookmark.create!(comment: "avoir top top!", movie: Movie.find_by(title: "Titanic"), list: list)
+puts "Finished!"
